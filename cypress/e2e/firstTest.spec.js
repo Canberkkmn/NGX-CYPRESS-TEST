4abaf3210e6f04f53ad15e3c4fe2ff44259e496f
Canberkkmn <canberkkaman@gmail.com>
2024-10-09 20:58:19 +0300

    initial commit

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+
+describe('First test suite', () => {
+    it('first test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //by Tag name
+        cy.get('input')
+
+        // by ID
+        cy.get('#inputEmail1')
+
+        //by Class value
+        cy.get('.input-full-width')
+
+        //by two attributes
+        cy.get('[placeholder="Email"][fullwidth]')
+
+        //by tag, attribute id and class
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        //by cypress test ID
+        cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card', 'Horizontal form').find('button')
+        cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+        cy.contains('nb-card', 'Horizontal form').get('button')
+
+        // chaining
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+    })
+
+    it.only('save subject of the command', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        // alias method
+        cy.contains('nb-card', 'Using the Grid').as('usingTheGrid')
+        cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        // then() method
+        cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm => {
+            cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
+            cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+        })
+    })
+})

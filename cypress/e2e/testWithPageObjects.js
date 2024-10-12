0bd6609a776f36133ab5a956aac21710465cb113
Canberkkmn <canberkkaman@gmail.com>
2024-10-12 20:39:14 +0300

    test: add page objects

---
+++ cypress/e2e/testWithPageObjects.js
@@ -0,0 +1,15 @@
+import {  navigateTo } from "../support/page_objects/navigationPage"
+
+describe('Test with Page Objects', () => {
+    beforeEach('open application', () => {
+        cy.visit('/')
+    })
+
+    it('verify navigation across the pages', () => {
+        navigateTo.formLayoutsPage()
+        navigateTo.datepickerPage()
+        navigateTo.smartTablePage()
+        navigateTo.toasterPage()
+        navigateTo.tooltipPage()
+    })
+})

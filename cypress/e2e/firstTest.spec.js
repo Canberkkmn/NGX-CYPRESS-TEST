70c2f58970120493d759f27c779af06090d60653
Canberkkmn <canberkkaman@gmail.com>
2024-10-12 20:07:46 +0300

    test: add tooltip & alert

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -118,7 +118,7 @@ describe('First test suite', () => {
         cy.get('[type="checkbox"]').eq(1).click({ force: true })
     })
 
-    it.only('Web Tables', () => {
+    it('Web Tables', () => {
         cy.visit('/')
         cy.contains('Tables & Data').click()
         cy.contains('Smart Table').click()
@@ -157,6 +157,27 @@ describe('First test suite', () => {
                     cy.wrap(tableRow).find('td').eq(6).should('contain', age)
                 }
             })
+        })
+    })
+
+    it('tooltips', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+
+        cy.contains('nb-card', 'Colored Tooltips').contains('Default').click()
+        cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+    })
+
+    it.only('dialog box (alerts and confirm boxes)', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
         })
     })
 })

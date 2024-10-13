f7a0946644b7c1ea5d96f9244b86fb2b16173986
Canberkkmn <canberkkaman@gmail.com>
2024-10-13 18:57:11 +0300

    test: add form layouts page and smart table page tests

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,4 +1,6 @@
-import {  navigateTo } from "../support/page_objects/navigationPage"
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
+import { navigateTo } from "../support/page_objects/navigationPage"
+import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 
 describe('Test with Page Objects', () => {
     beforeEach('open application', () => {
@@ -11,5 +13,16 @@ describe('Test with Page Objects', () => {
         navigateTo.smartTablePage()
         navigateTo.toasterPage()
         navigateTo.tooltipPage()
+    })
+
+    it('should submit Inline and Basic form', () => {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('Artem', 'test@test.com')
+        onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password')
+
+        navigateTo.smartTablePage()
+        onSmartTablePage.addNewRecordWithFirstAndlastName('Artem', 'Bondar')
+        onSmartTablePage.updateAgeByFirstName('Artem', '35')
+        onSmartTablePage.deleteRowByIndex(1)
     })
 })
